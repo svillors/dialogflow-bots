ca90884a1fe880c92477ad69c1d8545934bcbc76
svillors <tntzator9@gmail.com>
2025-05-05 16:30:16 +0500

    add DialogFlow learning script.

---
+++ teach_dialogflow.py
@@ -0,0 +1,48 @@
+import json
+import os
+
+from google.cloud import dialogflow
+from dotenv import load_dotenv
+
+
+def create_intent(project_id, display_name, training_phrases_parts, message_texts):
+    intents_client = dialogflow.IntentsClient()
+
+    parent = dialogflow.AgentsClient.agent_path(project_id)
+    training_phrases = []
+    for training_phrases_part in training_phrases_parts:
+        part = dialogflow.Intent.TrainingPhrase.Part(text=training_phrases_part)
+        training_phrase = dialogflow.Intent.TrainingPhrase(parts=[part])
+        training_phrases.append(training_phrase)
+
+    text = dialogflow.Intent.Message.Text(text=message_texts)
+    message = dialogflow.Intent.Message(text=text)
+
+    intent = dialogflow.Intent(
+        display_name=display_name, training_phrases=training_phrases, messages=[message]
+    )
+
+    response = intents_client.create_intent(
+        request={"parent": parent, "intent": intent}
+    )
+
+    print("Intent created: {}".format(response))
+
+
+def main():
+    load_dotenv()
+    project_id = os.environ['PROJECT_ID']
+    path_to_json = os.environ['PATH_TO_JSON']
+    with open(path_to_json, 'r') as raw_file:
+        file = json.load(raw_file)
+        for item in file:
+            create_intent(
+                project_id,
+                item,
+                file[item]['questions'],
+                [file[item]['answer']]
+            )
+
+
+if __name__ == '__main__':
+    main()
