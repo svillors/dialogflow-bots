9ddf0bc91fae817094a07242c38163e668b5b04f
svillors <tntzator9@gmail.com>
2025-05-07 01:56:00 +0500

    Add logging in tg bot

--- tg_bot.py
+++ tg_bot.py
@@ -1,9 +1,31 @@
 import os
+import logging
 
 from dotenv import load_dotenv
 from google.cloud import dialogflow
 from telegram import Update, ForceReply
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+
+logger = logging.getLogger('tg_bot_logger')
+
+
+class TelegramBotHandler(logging.Handler):
+
+    def __init__(self, updater, admin_id):
+        super().__init__()
+        self.udpater = updater
+        self.admin_id = admin_id
+
+    def emit(self, record):
+        if record.exc_info:
+            exc_type, exc_value, _ = record.exc_info
+            exc_name = exc_type.__name__
+            bot = self.udpater.bot
+            bot.send_message(
+                chat_id=self.admin_id,
+                text=f"Бот упал с ошибкой:\n\n{exc_name} - {exc_value}"
+            )
 
 
 def detect_intent_texts(project_id, session_id, text, language_code='ru'):
@@ -44,14 +66,22 @@ def main() -> None:
     load_dotenv()
     token = os.environ['TG_BOT_TOKEN']
     project_id = os.environ["PROJECT_ID"]
+    admin_id = os.environ["TG_ADMIN_ID"]
     updater = Updater(token)
+
+    logger.setLevel(logging.ERROR)
+    logger.addHandler(TelegramBotHandler(updater, admin_id))
+
     dispatcher = updater.dispatcher
     dispatcher.bot_data['project_id'] = project_id
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, answer_by_dialogflow))
-    updater.start_polling()
-    updater.idle()
+    try:
+        updater.start_polling()
+        updater.idle()
+    except Exception as e:
+        logger.error(e, exc_info=True)
 
 
 if __name__ == '__main__':
